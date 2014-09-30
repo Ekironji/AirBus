904be95fbdc70daf637b73c86081014469b53137
Ekironji <luca.pisani@aidilab.it>
2014-09-30 19:35:59 +0200

    Impl. grid standard in film

--- src/com/aidilab/airbus/fragment/FilmFragment.java
+++ src/com/aidilab/airbus/fragment/FilmFragment.java
@@ -1,6 +1,7 @@
 package com.aidilab.airbus.fragment;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,7 +9,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,18 +16,23 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import com.aidilab.airbus.R;
+import com.aidilab.airbus.gridview.GridObject;
+import com.aidilab.airbus.gridview.GridViewCustomAdapter;
 
-public class FilmFragment extends Fragment implements OnClickListener{
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
+public class FilmFragment extends Fragment{
+
+	final String DEBUG = "Shopping Fragment";
+	View rootView;
+	GridView itemsGridView;
+	ArrayList<String> categories;
+	HashMap<String, ArrayList<GridObject>> globalMap;
+	
     public static FilmFragment newInstance() {
     	FilmFragment fragment = new FilmFragment();
         return fragment;
@@ -35,80 +40,57 @@ public class FilmFragment extends Fragment implements OnClickListener{
 
     public FilmFragment() {
     }
-
-    
-    private ListView titleListview = null;
-    View rootView;
-    ImageView previewImage = null;
-    
+   
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_film, container, false);
         
-        final ListView categoryListview = (ListView) rootView.findViewById(R.id.categoriesListview);
-        titleListview    = (ListView) rootView.findViewById(R.id.titleListview);
-        previewImage     = (ImageView) rootView.findViewById(R.id.previewView);
+        ListView categoryListview = (ListView) rootView.findViewById(R.id.categoriesListviewFilm);
+        itemsGridView = (GridView) rootView.findViewById(R.id.gridViewCustomFilm);
         
+        categories = new ArrayList<String>();
+        categories.add("Action");
+        categories.add("Cartoon");
+        categories.add("Commedy");
+        categories.add("Series");
+        categories.add("Thriller");
         
-        String[] values = new String[] { "Action", "Cartoon", "Commedy", "Series", "Thriller"};
-         
-        String[] action    = new String[] {"Superman Returns", "Indiana Jones"};
-        String[] cartoon   = new String[] {"Aladdin", "The Lion King"};
-        String[] commedy   = new String[] {"The Hangover"};
-        String[] series    = new String[] {};
-        String[] thriller  = new String[] {};
+        globalMap = new HashMap<String, ArrayList<GridObject>>();
+        globalMap.put("Action", new ArrayList<GridObject>(Arrays.asList(new GridObject("Superman Returns", "€ 60", R.drawable.movie1),
+        		new GridObject("Indiana Jones", "€ 80", R.drawable.movie2))));
         
-        final int[] copertine = {R.drawable.movie1,
-        		R.drawable.movie2,
-        		R.drawable.movie3,
-        		R.drawable.movie4,
-        		R.drawable.movie5 };
+        globalMap.put("Cartoon", new ArrayList<GridObject>(Arrays.asList(new GridObject("Aladdin", "€ 40", R.drawable.movie3),
+        		new GridObject("The Lion King", "€ 70", R.drawable.movie4))));
         
-        final ArrayList<String[]> titoli = new ArrayList<String[]>();
-        titoli.add(action);
-        titoli.add(cartoon);
-        titoli.add(commedy);
-        titoli.add(series);
-        titoli.add(thriller);
-        
-        if (rootView != null) {
-            ImageButton mButton = (ImageButton) rootView.findViewById(R.id.playButton);
-            Log.d("", "View is not null");
+        globalMap.put("Commedy", new ArrayList<GridObject>(Arrays.asList(new GridObject("The Hangover", "€ 350", R.drawable.movie5))));
+        globalMap.put("Series", new ArrayList<GridObject>());
+        globalMap.put("Thriller", new ArrayList<GridObject>());
 
-            if (mButton != null) {
-            	mButton.setOnClickListener(this);
-                Log.d("", "mButton is not null");
-            }
-        }
-        
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-        	list.add(values[i]);
-        }
-        final StableArrayAdapter adapter = new StableArrayAdapter(rootView.getContext(),
-            android.R.layout.simple_list_item_1, list);
-        categoryListview.setAdapter(adapter);
+        categoryListview.setAdapter(new StableArrayAdapter(rootView.getContext(), 
+        		android.R.layout.simple_list_item_1, categories));
         
         categoryListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                 int position, long id) {
               final String item = (String) parent.getItemAtPosition(position);
-              updateTitleList(titoli.get(position));
+//              Log.i(DEBUG, "item: " + item);
+              updateGridView(item);
             }
 
-          });
+        });
         
-        titleListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                int position, long id) {
-              final String item = (String) parent.getItemAtPosition(position);
-              previewImage.setImageResource(copertine[position]);
-            }
+        itemsGridView.setAdapter(new GridViewCustomAdapter(rootView.getContext(), globalMap.get("Action")));
+        
+        itemsGridView.setOnItemClickListener(new OnItemClickListener() {
 
-          });
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
+               GridObject selected = (GridObject) itemsGridView.getItemAtPosition(position);
+               
+            }
+           });
         
         return rootView;
     }
@@ -121,14 +103,8 @@ public class FilmFragment extends Fragment implements OnClickListener{
     
     
     
-    private void updateTitleList(String[] values){
-    	final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-        	list.add(values[i]);
-        }
-        final StableArrayAdapter adapter = new StableArrayAdapter(rootView.getContext(),
-            android.R.layout.simple_list_item_1, list);
-        titleListview.setAdapter(adapter);
+    private void updateGridView(String category){
+    	 itemsGridView.setAdapter(new GridViewCustomAdapter(rootView.getContext(), globalMap.get(category)));
     }
     
     
@@ -154,15 +130,5 @@ public class FilmFragment extends Fragment implements OnClickListener{
           return true;
         }
     }
-
-
-	@Override
-	public void onClick(View v) {
-		Log.i("filmFragment","onClick");
-		if(v.getId() == (R.id.playButton)){
-			Intent LaunchIntent = getActivity().getPackageManager().getLaunchIntentForPackage("org.videolan.vlc.betav7neon");
-		    startActivity(LaunchIntent);
-		}
-	}
       
 }
